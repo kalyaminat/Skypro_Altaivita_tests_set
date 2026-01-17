15e8ef355796998ffb34407625fa27ea92cc76d9
Tatiana Kalyamina <116824949+kalyaminat@users.noreply.github.com>
2026-01-17 18:54:21 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,16 +2,6 @@ import pytest
 import allure
 from selenium import webdriver
 
-# @pytest.fixture
-# def browser():
-#     with allure.step("Открыть и настроить браузер"):
-#         browser = webdriver.Chrome()
-#         browser.implicitly_wait(4)
-#         browser.maximize_window()
-#         yield browser
-#     with allure.step("Закрыть браузер"):
-#         browser.quit()
-
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
